eb80de946f4f0c93fd930eefeda2f04778486c56
Diego Boyatjian <dboyatjian@gmail.com>
2023-04-05 00:58:46 -0300

    agregado de id a los botones, prueba del js

---
+++ js/mokepon.js
@@ -0,0 +1,8 @@
+
+function seleccionarMascotaJugador(){
+    console.log('se selecciono')
+    alert("se selecciono tu mascota")
+}
+
+let botonMascotaJugador = document.getElementById('boton-seleccionar-mascota');
+botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)

ae91c1412823821fad135be1fe5d0d04143aed84
Diego Boyatjian <dboyatjian@gmail.com>
2023-04-16 00:56:51 -0300

    mejora en la carga del documento y sus elementos

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,8 +1,15 @@
 
+function iniciarJuego(){
+    // reacciona al elegir al mokepon
+    console.log('cargo OK el juego')
+    let botonMascotaJugador = document.getElementById('boton-seleccionar-mascota');
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+}
+
 function seleccionarMascotaJugador(){
     console.log('se selecciono')
     alert("se selecciono tu mascota")
 }
 
-let botonMascotaJugador = document.getElementById('boton-seleccionar-mascota');
-botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+// luego de que se carga todo el HTML, inicia el juego
+window.addEventListener('load', iniciarJuego)

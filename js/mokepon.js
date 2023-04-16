a7fe48d743a8021b6ab8e1aacf4284a3f9f95fc9
Diego Boyatjian <dboyatjian@gmail.com>
2023-04-16 05:46:41 -0300

    seleccion del Mokepon Random del PC

--- js/mokepon.js
+++ js/mokepon.js
@@ -14,27 +14,45 @@ const LANGOSTELVIS_ID = "langostelvis";
 const TUCAPALMA_ID = "tucapalma";
 const PYDOS_ID = "pydos";
 const MOKEPONES_ID = [HIPODOGE_ID, CAPIPEPO_ID, RATIGUEYA_ID, LANGOSTELVIS_ID, TUCAPALMA_ID, PYDOS_ID];
+const MAX_ARRAY_MOKE = MOKEPONES_ID.length;
+const MIN_ARRAY_MOKE = 1;
 
-function iniciarJuego(){
-    // reacciona al elegir al mokepon
+function iniciarJuego() {
     console.log('cargo OK el juego')
+
+    // reacciona al elegir al mokepon
     let botonMascotaJugador = document.getElementById('boton-seleccionar-mascota');
     botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
 }
 
-function seleccionarMascotaJugador(){
+function seleccionarMascotaJugador() {
     let mascota = MOKEPONES_ID.filter(element => document.getElementById(element).checked === true);
-    
-    if (mascota.length == 0){
+
+    if (mascota.length == 0) {
         console.error("No se ha elegido un Mokepon")
         alert("Debes seleccionar un Mokepon!")
     } else {
         //Setea primera mayuscula
-        mascota = mascota[0].charAt(0).toUpperCase()+mascota[0].substring(1);
+        mascota = formatearNombre(mascota[0]);
         console.log('Has seleccionado a ' + mascota)
         // modifica el HTML de forma dinamica
         document.getElementById('mascota-jugador').innerHTML = mascota;
+
+        // PC elige mokepon
+        seleccionarMascotaPC();
     }
+}
+
+function formatearNombre(mascota) {
+    return mascota.charAt(0).toUpperCase() + mascota.substring(1);
+}
+
+function seleccionarMascotaPC() {
+    console.log('Se elige el Mokepon de la PC');
+    let numRandom = Math.floor(Math.random() * (MAX_ARRAY_MOKE - MIN_ARRAY_MOKE - 1) + MIN_ARRAY_MOKE)
+    console.log('numero random', numRandom, ', mokepon=', MOKEPONES_ID[numRandom])
+    let mascotaPC = formatearNombre(MOKEPONES_ID[numRandom]);
+    document.getElementById('mascota-PC').innerHTML = mascotaPC;
 }
 
 // luego de que se carga todo el HTML, inicia el juego
